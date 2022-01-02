5dd262e7e9d9d376383337ecc1c0d38192281d1b
vkumar825 <vkumar825@users.noreply.github.com>
2022-01-02 13:41:55 -0500

    fixed typos

--- client.c
+++ client.c
@@ -23,23 +23,23 @@ int main()
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    printf("\n Connecting to server...\n")
+    printf("\n Connecting to server...\n");
 
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("Connection Failed!\n")
+        printf("Connection Failed!\n");
         return 1;
     }
 
     else
     {
-        printf("Connection Successful!\n")
+        printf("Connection Successful!\n");
     }
 
     send(sockfd, testMessage, strlen(testMessage), 0);
     recv(sockfd, buffer, 1024, 0);
-    printf("Recieved from Server: %s\n", buffer)
+    printf("Recieved from Server: %s\n", buffer);
     return 0;
 }
